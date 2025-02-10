b5ccdc4062dc9dd3fec4ae9863d39a848bf09758
timmi <timmid999@gmail.com>
2025-02-09 23:17:35 -0800

    Implemented Repository tests. Most of the repositories have no methods, therefore do not need tests, but if methods are implemented in the future the files are already created.

---
+++ backend/task-manager/src/test/java/com/example/task_manager/repository_tests/TaskRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.example.task_manager.repository_tests;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class TaskRepositoryTest {
+
+}
