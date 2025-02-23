8148f89af54c1d2f785e47bc29dadb086fed2789
lucywalt <lucywltrs@gmail.com>
2025-02-23 15:31:15 -0800

    added login page and login style sheet

--- frontend/react-app/src/Login.js
+++ frontend/react-app/src/Login.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import './login.css';
 
 const Login = () => {
     return (
-        <form>
-            <label>Login</label>
+        <body id="login">
+            <div class="container">
 
-            <input type="text" name="userName" placeholder="Username"/>
+                <div id="siteName">
+                    <img src="https://www.shutterstock.com/image-vector/sample-logo-business-brand-identity-600nw-1948366864.jpg" alt="logo"/>
+                    <h1>Our site name</h1>
+                    <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum nulla pharetra, ultrices mauris ut, pharetra massa. Morbi sodales in metus in porta. Morbi vel semper ligula, at dignissim eros. Aenean sollicitudin, diam et cursus elementum, elit dui viverra leo, et commodo tortor urna et dolor</p1>
+                </div>
 
-        </form>
+
+                <form id="loginForm">
+                    <label>Log in</label>
+                    
+                    <input type="text" name="username" placeholder="Username"/>
+                    
+                    <input type="password" name="password" placeholder="Password"/>
+                    
+                    <button type="submit">Login</button>
+                
+                </form>
+            </div>
+        </body>
     )
 };
 
