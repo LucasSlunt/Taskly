ed079217b7190a92fad87c87f23f147850a3b0c8
lucywalt <lucywltrs@gmail.com>
2025-02-23 14:02:15 -0800

    Added page routing to App.js, removed demo page from App.js

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react';
+
+//routing for pages?
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//Login page
+import Login from'./Login';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <a href="/login">Login</a>
+      </nav>
+
+      <Routes>
+        <Route path="/Login" element={<Login/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/react-app/src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <form>
+            <label>Login</label>
+
+            <input type="text" name="userName" placeholder="Username"/>
+
+        </form>
+    )
+};
+
+export default Login;
