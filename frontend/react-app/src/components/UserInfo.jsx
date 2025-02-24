55b5ad7be737f80500b118a4c4070adf9098f14b
Ivona Nicetin <ivona.nicetin@gmail.com>
2025-02-24 00:27:33 -0800

    made a user info file

---
+++ frontend/react-app/src/components/UserInfo.jsx
@@ -0,0 +1,25 @@
+import "../css/Profile.css"
+import logo from '../logo.svg';
+
+function UserInfo(){
+    const user  = {
+        name: "Bobby Joe",
+        username: "bobbyjoe",
+        email: "bobbyjoe@example.com",
+        profilePictureUrl: "https://i.pravatar.cc/150?img=12" 
+    }
+    return (
+        <div className="user-info">
+            <div className="profile-picture">
+                    <img src={user.profilePictureUrl} alt="Profile Picture" />
+                </div>
+                <div className="profile-info">
+                    <h2 className="profile-name">{user.name}</h2>
+                    <p className="profile-username">{user.username}</p>
+                    <p className="profile-email">{user.email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default UserInfo
--- frontend/react-app/src/pages/Profile.jsx
+++ frontend/react-app/src/pages/Profile.jsx
@@ -1,19 +1,11 @@
 import "../css/Profile.css"
-import logo from '../logo.svg';
+
+import UserInfo from "../components/UserInfo"
 
 function Profile(){
     return (
     <div className="profile-page">
-        <div className="profile-header">
-            <div className="profile-picture">
-                <img src={logo} alt="Profile Picture" />
-            </div>
-            <div className="profile-info">
-                <h2 className="profile-name">Jane Doe</h2>
-                <p className="profile-username">janedoe123</p>
-                <p className="profile-email">janedoe@example.com</p>
-            </div>
-        </div>
+        <UserInfo />
     </div>
     
     );
