437a2cff98160b33bef75303c6bc8424f144de54
lucywalt <lucywltrs@gmail.com>
2025-02-27 08:52:17 -0800

    Added component to task.jsx

--- frontend/react-app/src/Task.jsx
+++ frontend/react-app/src/Task.jsx
@@ -1,1 +1,11 @@
 import React from 'react';
+
+const Task = () => {
+    return (
+        <tr>
+            <td className="taskName">Create wireframe</td>
+            <td><button className="miniButton">Team 1</button></td>
+            <td>11 04 25</td>
+        </tr>
+    )
+}
