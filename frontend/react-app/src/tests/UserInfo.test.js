f80a5291ab3f8152527c380852fabcc8d56f381a
Ivona Nicetin <ivona.nicetin@gmail.com>
2025-02-26 22:22:59 -0800

    re added profile page files

---
+++ frontend/react-app/src/tests/UserInfo.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import UserInfo from "../components/UserInfo";
+
+test("renders user info with name, username, email, and profile picture", () => {
+    const { getByAltText, getByText } = render(<UserInfo />);
+
+    //check if name, username, and email are displayed
+    expect(getByText("Bobby Joe")).toBeInTheDocument();
+    expect(getByText("bobbyjoe")).toBeInTheDocument();
+    expect(getByText("bobbyjoe@example.com")).toBeInTheDocument();
+
+    //check if profile picture is rendered
+    const profileImage = getByAltText("Profile Picture");
+    expect(profileImage).toBeInTheDocument();
+    expect(profileImage).toHaveAttribute("src", "https://i.pravatar.cc/150?img=12");
+    expect(profileImage).toHaveAttribute("alt", "Profile Picture");
+});
