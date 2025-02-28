875ad095c6cdb6c2b8ce1840234f0ff6c247c377
lucywalt <lucywltrs@gmail.com>
2025-02-27 18:09:06 -0800

    Revert "Referenced task in home page"
    This reverts commit 09a945f60739cd05a9d51f73100d033b0c9e73a9.

--- frontend/react-app/src/Task.jsx
+++ frontend/react-app/src/Task.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-
-const Task = ({taskName, taskTeam, dueDate}) => {
+const Task = () => {
     return (
         <tr>
-            <td className="taskName">{taskName}</td>
-            <td><button className="miniButton">{taskTeam}</button></td>
-            <td>{dueDate}</td>
+            <td className="taskName">Create wireframe</td>
+            <td><button className="miniButton">Team 1</button></td>
+            <td>11 04 25</td>
         </tr>
     )
 }
-
-export default Task;
