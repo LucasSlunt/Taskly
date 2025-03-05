308fb03fd4695449e9349c4d50022eac38aaf0b7
James Birnie <jkbirnie@gmail.com>
2025-03-05 09:23:30 -0800

    started on a on sumbit function to make json

--- frontend/react-app/src/pages/CreateTask.jsx
+++ frontend/react-app/src/pages/CreateTask.jsx
@@ -1,56 +1,66 @@
 import fakeTeamData from "../FakeData/fakeTeamData.json"
 import Header from "../Header"
 function CreateTask(){
-    function createJSONFromForm(){
-        let name = document.getElementById("name").value
-            
+    function task(name, assignees, priority, discription, images){
+        this.name = name
+        this.assignees = assignees
+        this.priority = priority
+        this. discription = discription
+        this.images = images
     }
+    const handleSubmit = (event) => {
+        //newTask = task(
+        //    event.target.name.value,
+        //    )
+        console.log(event.target.name.value)          // or directly
+      }
         
 
     return (
         <div>
             <Header/>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <label>
                     Task Name
-                </label>
-                <div>
-                <input type="text" name="Name Of Task" id="name" />
-                </div>
-                <div>
+                    <div>
+                    <input type="text" name="Name Of Task" id="name" required/>
+                    </div>
+                    </label>
+                <label>
                     Assign To:
-                </div>
                 {fakeTeamData.map((teamMember)=>(
                     <div>
                         {teamMember.name}
                         <input type="checkbox" name="checkTeamMemeber" id={teamMember.id} />
                     </div>
                 ))}
-                <div>
+                </label>
+                <label>
                     Set Priority
-                </div>
+                
                 <div>
-                    <select name="setPrioity" id="setPrioity">
+                    <select name="setPrioity" id="setPrioity" required>
                         <option value="Low">Low</option>
                         <option value="Medium">Medium</option>
                         <option value="High">High</option>
                     </select>
                 </div>
-                <div>
+                </label>
+                <label>
                     Add Discription
-                </div>
+                
                 <div>
                     <input type="text" name="input-discription" id="discription" />
                 </div>
-                <div>
+                </label>
+                <label>
                     Add Images
-                </div>
+                
                 <div>
                     <input type="file" name="input-photos" id="photos" multiple accept="image/*"/>
                 </div>
-                <div>
-                    <input type="button" value="Create Task" onClick={createJSONFromForm()}/>
-                </div>
+                </label>
+                <input type="submit" value="Create Task" onClick={handleSubmit()}/>
                 </form>
         </div>
     
