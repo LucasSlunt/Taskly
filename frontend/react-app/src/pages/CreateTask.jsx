f6ccd4338328d9ea5ff77f4149e24fd3b4f16aad
James Birnie <jkbirnie@gmail.com>
2025-03-06 14:54:43 -0800

    Added test as well as finished the page moved form into a coponent

--- frontend/react-app/src/pages/CreateTask.jsx
+++ frontend/react-app/src/pages/CreateTask.jsx
@@ -1,79 +1,21 @@
 import fakeTeamData from "../FakeData/fakeTeamData.json"
 import Header from "../Header"
-
-import {useForm, Controller} from 'react-hook-form'
+import CreateTaskForm from "../components/CreateTaskForm"
+import "../css/CreateTask.css"
 function CreateTask(){
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => {
-        console.log(data)
-    };
-
-
-        
 
     return (
-        <div>
-            <Header/>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label>
-                    Task Name
-                    <div>
-                    <input type="text" name="name" id="name"{...register("name", { required: true })}/>
-                    </div>
-                    </label>
-                <label>
-                    Assign To:
-                {fakeTeamData.map((teamMember)=>(
-                    <div>
-                        <Controller
-                            name="myCheckbox"
-                            control={control}
-                            defaultValue={false}
-                            render={({ field }) => (
-                            <FormControlLabel
-                                control={
-                                <Checkbox
-                                    {...field}
-                                    checked={field.value}
-                                    onChange={(e) => field.onChange(e.target.checked)}
-                                />
-                                }
-                                label={teamMember.name}
-                            />
-                            )}
-                        />
-                    </div>
-                ))}
-                </label>
-                <label>
-                    Set Priority
-                
-                <div>
-                    <select name="priority" id="priority" required {...register("priority", { required: true })}>
-                        <option value="Low">Low</option>
-                        <option value="Medium">Medium</option>
-                        <option value="High">High</option>
-                    </select>
+        <div className="page">
+            <div className="pageBox">
+                <Header/>
+                <div className="body">
+                    <CreateTaskForm
+                    team={fakeTeamData}
+                    />
                 </div>
-                </label>
-                <label>
-                    Add Discription
-                
-                <div>
-                    <input type="text" name="input-discription" id="discription" {...register("priority", { required: false })}/>
-                </div>
-                </label>
-                <label>
-                    Add Images
-                
-                <div>
-                    <input type="file" name="input-photos" id="photos" multiple accept="image/*"/>
-                </div>
-                </label>
-                <input type="submit" value="Create Task"/>
-                </form>
+                    
+            </div>
         </div>
-    
     );
 }
 
