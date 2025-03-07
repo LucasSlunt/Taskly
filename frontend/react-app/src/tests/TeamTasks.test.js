a655ed3c7b9d0f98b04093d2c376eae62d062023
Ivona Nicetin <ivona.nicetin@gmail.com>
2025-03-07 00:24:07 -0800

    button styling

--- frontend/react-app/src/tests/TeamTasks.test.js
+++ frontend/react-app/src/tests/TeamTasks.test.js
@@ -2,14 +2,16 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import TeamTasks from "../pages/TeamTasks";
 
-jest.mock("../components/TeamMember", () => () => <div data-testid="members">Mock Team Member</div>);
+jest.mock("../components/TeamMember", () => () => <div data-testid="team-members">Mock Team Member</div>);
+
+
 
 test('renders TeamTasks page with mocked TeamMember for each member', () => {
   render(<TeamTasks />);  // Render the TeamTasks component
   
   //Check if each mocked TeamMember is rendered for every member in the list
-  const memberElements = screen.getAllByTestId('members'); 
+  const memberElements = screen.getAllByTestId('team-members'); 
   expect(memberElements).toHaveLength(5); 
-  
 
 });
+
