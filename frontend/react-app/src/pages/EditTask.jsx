7365ef89af67639b98bf1fe5221b11b8190b15c9
James Birnie <jkbirnie@gmail.com>
2025-03-10 16:11:26 -0700

    added testing and moved the form into its own compent

--- frontend/react-app/src/pages/EditTask.jsx
+++ frontend/react-app/src/pages/EditTask.jsx
@@ -1,5 +1,5 @@
 import {Link, useLocation} from 'react-router-dom'
-import {useForm} from 'react-hook-form'
+import EditTaskForm from '../components/EditTaskForm'
 import Header from '../components/Header'
 import '../css/EditTask.css'
 function EditTask(){
@@ -9,57 +9,14 @@ function EditTask(){
         console.log("taskToEdit: ", taskToEdit)
         console.log("onThisTeam: ", onThisTeam)
 
-        const { register, handleSubmit, formState: {errors}} = useForm();
-        const onSubmit = data => {
-            console.log(data)
-            alert("Your task was updated")
-            window.location.href="/home";
-        };
         return(
             <div className='page'>
                 <Header/>
                 <div>
-                    <form onSubmit={handleSubmit(onSubmit)} className='body'>
-                        <label className='majorLabel'>
-                                Task Name
-                                <div>
-                                <input type="text" name="name" id="name" defaultValue={taskToEdit.name}{...register("name", { 
-                                    required:{
-                                        value: true,
-                                        message: 'Please set a Task Name'
-                                    }
-                                    })}/>
-                                </div>
-                                </label>
-                            <label className='majorLabel'>
-                            <div>
-                                {
-                                    errors.name && <span>{errors.name.message}</span>
-                                }
-                            </div>
-                    
-                            </label>
-                            <label className='majorLabel'>
-                                Assign To:
-                                <div className='Checkboxs'>
-                                    {onThisTeam.map((teamMember)=>(
-                                        <div className='checkbox' key = {teamMember.name}>
-                                            {teamMember.name}
-                                            <input type="checkbox" name="" id="" value = {teamMember.name} {...register("assignees")}/>
-                                        </div>
-                                    ))}
-                                </div>
-                            </label>
-                            <label className='majorLabel'>
-                            Discription
-                            
-                            <div>
-                                <input type="text" name="input-discription" id="discription" defaultValue={taskToEdit.discription}  {...register("discription", { required: false })}/>
-                            </div>
-                            </label>
-                            <input type="submit" value="Create Task"/>
-                        
-                    </form>
+                    <EditTaskForm
+                    task={taskToEdit}
+                    team={onThisTeam}
+                    />
                 </div>
             </div>
         );
