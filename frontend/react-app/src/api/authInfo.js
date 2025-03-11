4cd443a40c91f8ae56205f075ff0228c22714743
timmi <timmid999@gmail.com>
2025-03-10 22:30:01 -0700

    Beginning implementation of authInfo.js. Not finished yet, and test have not been committed to branch yet.

--- frontend/react-app/src/api/authInfo.js
+++ frontend/react-app/src/api/authInfo.js
@@ -1,3 +1,37 @@
 const BASE_URL = "http://localhost:8080/api/auth-info";
 
-//Currently no API endpoints implemented
+export const login = async (teamMemberId, password) => {
+    try {
+        const response = await fetch(`${BASE_URL}/login`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({teamMemberId, password})
+        });
+
+        if (!response.ok) {
+            console.error(`Login failed: ${response.status} ${response.statusText}`);
+            return null;
+        }
+
+        return await response.json();
+    }
+    catch (error) {
+        console.error("Error during login: ", error);
+        throw error;
+    }
+};
+
+export const isAdmin = async (teamMemberId) => {
+    try {
+        const response = await fetch(`${BASE_URL}`/is-admin, {
+            method: 'GET',
+            
+        })
+    }
+    catch (error) {
+        console.error("Error checking member role: ", error);
+        throw error;
+    }
+};
