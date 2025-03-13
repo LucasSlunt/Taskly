dd1daeac72bc7caa82d3b44e990fb7cefa7e80c3
lucywalt <lucywltrs@gmail.com>
2025-03-13 12:50:19 -0700

    created notifs file and css

---
+++ frontend/react-app/src/pages/Notifications.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Notifications.css';
+
+const Notifications = () => {
+    return(
+        <div id="notifContainer">
+            <h1>Hello test</h1>
+        </div>
+    )
+};
+
+export default Notifications;
