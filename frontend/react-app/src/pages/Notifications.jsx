48ae0d2b1ada5711786db375bf95f9fc5128ebda
lucywalt <lucywltrs@gmail.com>
2025-03-13 13:36:26 -0700

    Added css and static html

--- frontend/react-app/src/pages/Notifications.jsx
+++ frontend/react-app/src/pages/Notifications.jsx
@@ -4,7 +4,34 @@ import '../css/Notifications.css';
 const Notifications = () => {
     return(
         <div id="notifContainer">
-            <h1>Hello test</h1>
+            <table>
+                <thead>
+                    <td colspan="3">Notifications</td>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="3" class="subHeader">Unread</td>
+                    </tr>
+                    <tr>
+                        <td class="items"><input type="checkbox" name="read"/></td>
+                        <td class="items"><button>Team 2</button></td>
+                        <td class="notifDetails">Bob edited your task "Create wireframe"</td>
+                    </tr>
+                    <tr>
+                        <td class="items"><input type="checkbox" name="read"/></td>
+                        <td class="items"><button>Team 1</button></td>
+                        <td class="notifDetails">Mary completed your task "Code things"</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3" class="subHeader">Read</td>
+                    </tr>
+                    <tr>
+                        <td class="items"><input type="checkbox" name="read" checked/></td>
+                        <td class="items"><button>Team 1</button></td>
+                        <td class="notifDetails">Adam assigned you to "Code more"</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 };
