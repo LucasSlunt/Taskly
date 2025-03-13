f0972bb87991a1f05fff834fae540cc93d6e4135
lucywalt <lucywltrs@gmail.com>
2025-03-13 12:10:41 -0700

    Added createaccount html and basic css

--- frontend/react-app/src/pages/CreateAccount.jsx
+++ frontend/react-app/src/pages/CreateAccount.jsx
@@ -3,8 +3,19 @@ import '../css/CreateAccount.css';
 
 const CreateAccount = () => {
     return (
-        <div class="testMe">
-            <h1>Hello</h1>
+        <div id="createAccountContainer">
+            <form id="createAccountForm">
+                    <label>Create Account</label>
+                    
+                    <input type="text" name="username" placeholder="Username"/>
+
+                    <input type="email" name="email" placeholder="Email"/>
+                        
+                    <input type="password" name="password" placeholder="Password"/>
+                       
+                     <button type="submit">Create</button>
+
+                </form>
         </div>
     )
 }
