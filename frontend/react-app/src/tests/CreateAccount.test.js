523c7ea414eb781a8b7e5cde5712cf4eef380de3
James Birnie <jkbirnie@gmail.com>
2025-03-15 16:27:34 -0700

    All test are passing, delted, un-needed tests

--- frontend/react-app/src/tests/CreateAccount.test.js
+++
@@ -1,8 +0,0 @@
-import CreateAccount from "../pages/CreateAccount";
-import { render, screen } from "@testing-library/react";
-
-test("renders CreateAccount page", () => {
-
-    render(<CreateAccount/>);
-
-});
