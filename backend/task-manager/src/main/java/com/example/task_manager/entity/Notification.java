de83bd10daeccef302357ad98df0498c0ec66194
timmi <timmid999@gmail.com>
2025-03-16 22:41:15 -0700

    Created Notification entity and enum class that contains predefined messages for each notification depending on what the notification is.

---
+++ backend/task-manager/src/main/java/com/example/task_manager/entity/Notification.java
@@ -0,0 +1,104 @@
+package com.example.task_manager.entity;
+
+import java.time.LocalDateTime;
+
+import com.example.task_manager.enums.NotificationType;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int notificationId;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private boolean isRead = false;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id", nullable = true)
+    private Task task;
+
+    @ManyToOne
+    @JoinColumn(name = "team_member_id", nullable = false)
+    private TeamMember teamMember;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType type; //predefined messages, so the user does not decide what the message says
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Notification() {}
+
+    public Notification(NotificationType type, String message, Task task, TeamMember teamMember) {
+        this.type = type;
+        this.message = message;
+        this.task = task;
+        this.teamMember = teamMember;
+    }
+
+    public int getNotificationId() {
+        return notificationId;
+    }
+
+    public void setNotificationId(int notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isIsRead() {
+        return isRead;
+    }
+
+    public void setIsRead(boolean isRead) {
+        this.isRead = isRead;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public TeamMember getTeamMember() {
+        return teamMember;
+    }
+
+    public void setTeamMember(TeamMember teamMember) {
+        this.teamMember = teamMember;
+    }
+
+    public NotificationType getType() {
+        return type;
+    }
+
+    public void setType(NotificationType type) {
+        this.type = type;
+    }
+}
---
+++ backend/task-manager/src/main/java/com/example/task_manager/enums/NotificationType.java
@@ -0,0 +1,11 @@
+package com.example.task_manager.enums;
+
+public enum NotificationType {
+    TASK_TITLE_EDITED,
+    TASK_DESCRIPTION_EDITED,
+    TASK_STATUS_EDITED,
+    TASK_DUE_DATE_EDITED,
+    TASK_LOCK_STATUS_CHANGED,
+    TASK_ASSIGNED,
+    TEAM_ASSIGNED
+}
