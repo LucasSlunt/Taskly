afc6ee96938eeb66b3868bbaa0531a9336ad287f
lucywalt <lucywltrs@gmail.com>
2025-03-21 10:25:03 -0700

    Added delete button and cleaned up css

--- frontend/react-app/src/pages/Notifications.jsx
+++ frontend/react-app/src/pages/Notifications.jsx
@@ -6,29 +6,32 @@ const Notifications = () => {
         <div id="notifContainer">
             <table>
                 <thead>
-                    <td colspan="3">Notifications</td>
+                    <td colspan="4">Notifications</td>
                 </thead>
                 <tbody>
                     <tr>
-                        <td colspan="3" class="subHeader">Unread</td>
+                        <td colspan="4" class="subHeader">Unread</td>
                     </tr>
                     <tr>
                         <td class="items"><input type="checkbox" name="read"/></td>
-                        <td class="items"><button>Team 2</button></td>
+                        <td class="items"><button class="smallTeamButton">Team 2</button></td>
                         <td class="notifDetails">Bob edited your task "Create wireframe"</td>
+                        <td><button class='delete'>Delete</button></td>
                     </tr>
                     <tr>
                         <td class="items"><input type="checkbox" name="read"/></td>
-                        <td class="items"><button>Team 1</button></td>
+                        <td class="items"><button class="smallTeamButton">Team 1</button></td>
                         <td class="notifDetails">Mary completed your task "Code things"</td>
+                        <td><button class='delete'>Delete</button></td>
                     </tr>
                     <tr>
-                        <td colspan="3" class="subHeader">Read</td>
+                        <td colspan="4" class="subHeader">Read</td>
                     </tr>
                     <tr>
                         <td class="items"><input type="checkbox" name="read" checked/></td>
-                        <td class="items"><button>Team 1</button></td>
+                        <td class="items"><button class="smallTeamButton">Team 1</button></td>
                         <td class="notifDetails">Adam assigned you to "Code more"</td>
+                        <td><button class='delete'>Delete</button></td>
                     </tr>
                 </tbody>
             </table>
