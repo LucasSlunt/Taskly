92ae28aa468656bb4706c71b28c26e4fd544c37c
lucywalt <lucywltrs@gmail.com>
2025-03-28 09:17:11 -0700

    Updated mytasks and fixed login css

--- frontend/react-app/src/pages/Login.jsx
+++ frontend/react-app/src/pages/Login.jsx
@@ -4,14 +4,16 @@ import '../login.css';
 function Login(){
     return (
         <div id="login">
-            <div class="container">
+            <div className="loginContainer">
 
                 <div id="siteName">
                     <img src="https://www.shutterstock.com/image-vector/sample-logo-business-brand-identity-600nw-1948366864.jpg" alt="logo"/>
                     <h1>Our site name</h1>
                     <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum nulla pharetra, ultrices mauris ut, pharetra massa. Morbi sodales in metus in porta. Morbi vel semper ligula, at dignissim eros. Aenean sollicitudin, diam et cursus elementum, elit dui viverra leo, et commodo tortor urna et dolor</p1>
                 </div>
-                <LoginForm/>
+                <div id="loginFormWrapper"> 
+                    <LoginForm />
+                </div>
             </div>
         </div>
     )
