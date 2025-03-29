688849e4d1d60b422e09734fc192d17e463077b2
Ivona Nicetin <ivona.nicetin@gmail.com>
2025-03-28 18:15:53 -0700

    setUpData method refactored in Mytasks page

--- frontend/react-app/src/pages/MyTasks.jsx
+++ frontend/react-app/src/pages/MyTasks.jsx
@@ -12,35 +12,40 @@ import { useState, useEffect } from 'react';
 function getAssigneesNames(taskItem) {
     return taskItem.assignedMembers.map((member) => member.userName).join(", ");
 }
+function mapTaskItem(taskItem) {
+    return {
+      name: taskItem,
+      id: taskItem.taskId,
+      team: taskItem.teamId,
+      assignees: getAssigneesNames(taskItem),
+      dueDate: taskItem.dueDate || "No Due Date",
+    };
+  }
 
-function setUpData(results) {
+  function setUpData(results) {
     return results
     .filter((taskItem) => taskItem.status !== "Done")
-      .map((taskItem) => ({
-        id: taskItem.taskId,
-        name: taskItem,
-        team: taskItem.teamId,
-        assignees: getAssigneesNames(taskItem),
-        status: taskItem.status,
-        priority: taskItem.priority,
-        dueDate: taskItem.dueDate || "No Due Date",
-        isLocked: taskItem.isLocked.toString()
-      }));
-}
-
-function setUpDataCompleted(results) {
+      .map((taskItem) => {
+        const baseItem = mapTaskItem(taskItem);
+        return{
+          ...baseItem,
+          status: taskItem.status,
+          priority: taskItem.priority,
+          isLocked: taskItem.isLocked.toString()
+        };
+        
+      });
+  }
+  function setUpDataCompleted(results) {
     return results
       .filter((taskItem) => taskItem.status === "Done")
-      .map((taskItem) => ({
-        id: taskItem.taskId,
-        name: taskItem,
-        assignees: getAssigneesNames(taskItem),
-        priority: taskItem.priority,
-        status: taskItem.status,
-        dueDate: taskItem.dueDate || "No Due Date",
-        dateCompleted: taskItem.dateCompleted,
-        isLocked: taskItem.isLocked.toString()
-      }));
+      .map((taskItem) => {
+        const baseItem = mapTaskItem(taskItem);
+        return{
+          ...baseItem,
+          dateCompleted: taskItem.dateCompleted,
+        };
+      });
   }
 
 function MyTasks(){
