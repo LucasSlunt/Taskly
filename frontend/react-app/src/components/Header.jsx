1f45387345b3c558fe0c64a5eeaf10f810ffe052
James Birnie <jkbirnie@gmail.com>
2025-03-29 21:25:02 -0700

    Changed logo href

--- frontend/react-app/src/components/Header.jsx
+++ frontend/react-app/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
             
                 <h1 className ="inHeader">
                         <ul className ='list'>
-                            <li className ="logo list"><span><a href="#">Whatever we Call this</a></span></li>
+                            <li className ="logo list"><span><a href="/home">Whatever we Call this</a></span></li>
                             <li><span><a href="/home">Home</a></span></li>
                             <li><span><a href="/my-tasks">Tasks</a></span></li>
                             <li><span><a href="/profile">Profile</a></span></li>
