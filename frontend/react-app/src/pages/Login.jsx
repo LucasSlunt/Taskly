e5ca2ba89ce21463b83382156461b03375c32b5b
lucywalt <lucywltrs@gmail.com>
2025-04-01 14:59:22 -0700

    Imported logo

--- frontend/react-app/src/pages/Login.jsx
+++ frontend/react-app/src/pages/Login.jsx
@@ -7,7 +7,7 @@ function Login(){
             <div className="loginContainer">
 
                 <div id="siteName">
-                    <img src="https://www.shutterstock.com/image-vector/sample-logo-business-brand-identity-600nw-1948366864.jpg" alt="logo"/>
+                    <img src="COSC310_GroupProject/frontend/react-app/src/icons/taskboard_checks.png" alt="logo"/>
                     <h1>Our site name</h1>
                     <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum nulla pharetra, ultrices mauris ut, pharetra massa. Morbi sodales in metus in porta. Morbi vel semper ligula, at dignissim eros. Aenean sollicitudin, diam et cursus elementum, elit dui viverra leo, et commodo tortor urna et dolor</p1>
                 </div>
