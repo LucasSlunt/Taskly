417f884501b835a23ab43e211a63ef0060a7dbfb
timmi <timmid999@gmail.com>
2025-04-01 20:53:14 -0700

    Added icons, needs further refactoring.

--- frontend/react-app/src/components/Header.jsx
+++ frontend/react-app/src/components/Header.jsx
@@ -1,20 +1,25 @@
-import {useCookies} from 'react-cookie'
+import { useCookies } from 'react-cookie'
+import { Home, ListTodo, UserCircle, Settings, ListCheck, ListChecks, ShieldAlert, ShieldUser } from "lucide-react";
+
 function Header(){
     const [cookies] = useCookies(['userInfo'])
     return(
-           
-          <div>
-                <h1 className ="inner-header">
-                        <ul className ='list'>
-                            <li className ="logo list"><span><a href="/home">Whatever we Call this</a></span></li>
-                            <li><span><a href="/home">Home</a></span></li>
-                            <li><span><a href="/my-tasks">Tasks</a></span></li>
-                            <li><span><a href="/profile">Profile</a></span></li>
-                            {cookies.userInfo.role === 'admin' && (<li><span><a href="/admin-panel">Admin Controls</a></span></li>)}
-                        </ul>
-                </h1>
-            </div>
-            
+        <div>
+            <h1 className="inner-header">
+                <div className="logo" >
+                    <a href="/home">Taskly</a>
+                </div>
+
+                <ul className ='icon-nav'>
+                    <li><a href="/home"><Home/></a></li>
+                    <li><a href="/my-tasks"><ListChecks/></a></li>
+                    <li><a href="/profile"><UserCircle/></a></li>
+
+                    {cookies.userInfo.role === 'admin' && (
+                        <li><a href="/admin-panel"><ShieldUser /></a></li>)}
+                </ul>
+            </h1>
+        </div>
     );
 }
 export default Header
